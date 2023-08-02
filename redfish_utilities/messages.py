7d39e17866230b8e5624125fb0b25c0ec47860c6
peter.cy.chen <peter.cy.chen@aewin.com>
2023-08-03 03:43:06 +0800

    change implment
    move verify_response back to redfish_utilities/messages.py
    
    Signed-off-by: peter.cy.chen <peter.cy.chen@aewin.com>

--- redfish_utilities/messages.py
+++ redfish_utilities/messages.py
@@ -14,6 +14,25 @@ Brief : This file contains the definitions and functionalities for interacting
 
 from redfish.messages import *
 
+def verify_response( response ):
+    """
+    Verifies a response and raises an exception if there was a failure
+
+    Args:
+        response: The response to verify
+    """
+
+    if response.status >= 400:
+        messages_detail = get_messages_detail(response)
+        exception_string = get_error_messages( messages_detail )
+        message_item = search_message(messages_detail, "Base", "PasswordChangeRequired")
+        if not message_item is None:
+            raise RedfishPasswordChangeRequiredError( "Operation failed: HTTP {}\n{}".format( response.status, exception_string ), message_item["MessageArgs"][0])
+        else:
+            raise RedfishOperationFailedError( "Operation failed: HTTP {}\n{}".format( response.status, exception_string ) )
+
+    return
+
 """
     for backend capability
 """
